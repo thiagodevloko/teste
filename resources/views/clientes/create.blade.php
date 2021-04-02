0d72a6b03d0435e885b5bd78eb53bf39addaea00
Thiago <thiago-limasoares@hotmail.com>
2021-04-02 09:37:53 -0300

    Templates 2

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,8 +1,11 @@
-<h3>Novo Cliente</h3>
+@extends('layouts.principal')
 
-<form action="{{route('clientes.store')}}" method="POST">
-    @csrf
-    <label for="">Nome</label>  
-    <input type="text" name="nome">
-    <button type="submit">Cadastrar</button>
-</form>
+@section('conteudo')
+    <h3>Novo Cliente</h3>
+    <form action="{{route('clientes.store')}}" method="POST">
+        @csrf
+        <label for="">Nome</label>  
+        <input type="text" name="nome">
+        <button type="submit">Cadastrar</button>
+    </form>    
+@endsection
--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -3,14 +3,25 @@
 @section('conteudo')
 <h3>{{$titulo}}</h3>
 <a href="{{ route ('clientes.create')}}">Novo Cliente</a>
-
+<style>
+    li{
+        display: flex;
+        list-style: none;
+        padding-bottom: 5px;     
+    }
+    a{
+        padding-left: 10px;
+        margin-right: 10px;
+    }
+    
+</style>
 @if (count($clientes)>0)    
 <ul>
     @foreach ($clientes as $c)
     <li>
-        {{ $c['id']}} | {{$c['nome']}} 
-        | <a href="{{ route ('clientes.edit', $c['id'])}}">Editar</a>
-        | <a href="{{ route ('clientes.show', $c['id'])}}">Info</a>
+        {{ $c['id']}} | {{$c['nome']}} |
+        <a href="{{ route ('clientes.edit', $c['id'])}}">Editar</a>
+        <a href="{{ route ('clientes.show', $c['id'])}}">Info</a>
         
         <form action="{{route('clientes.destroy', $c['id'])}}" method="POST">
             @csrf
