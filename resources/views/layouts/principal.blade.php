be1e9b0a3d4117f088320bf02cd32e976e1088f2
Thiago <thiago-limasoares@hotmail.com>
2021-04-06 20:37:09 -0300

    Declarando estilos...

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -4,55 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/principal.css') }}">
     <title>Views</title>
-    <style>
-        .row {
-            display: flex;
-        }
-
-        .col1 {
-            flex: 30%;
-            padding: 10px;
-        }
-
-        .col2 {
-            flex: 70%;
-            padding: 10px;
-        }
-
-        .menu ul {
-            margin: 0px;
-            padding: 0px;
-            list-style-type: none;
-        }
-
-        .menu li {
-            list-style: none;
-        }
-
-        .menu a {
-            display: block;
-            width: 200px;
-            height: 20px;
-            color: black;
-            background-color: #FFFFFF;
-            text-decoration: none;
-            text-align: center;
-            margin: 5px;
-            font-size: 18px;
-        }
-
-        .menu a:hover {
-            background-color: #b9bcd0;
-            color: #0000ff;
-            font-weight: bold;
-        }
-
-        .menu .active {
-            background-color: #c0d4f3;
-        }
-
-    </style>    
 </head>
 <body>
     <div class="row">
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,7 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+// mix.styles([
+//         'resources/css/principal.css'
+//     ],'public/clientes/css.principal.css');
