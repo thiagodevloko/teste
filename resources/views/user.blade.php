6755a42f379f1ae56d25ea0325936bd0e5b80d13
thiagodevloko <thiago-limasoares@hotmail.com>
2021-05-05 19:24:45 -0300

    configurando git instalação nova

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -3,6 +3,6 @@
 <div>
     <a href="{{route('app.user')}}">USER</a>
     <a href="{{route('app.profile')}}">PROFILE</a>
-    <a href="{{route('app')}}">APLICATIVOS</a>
+    <a href="{{route('app')}}">APLICATIVO</a>
     <a href="{{route('meusprodutos')}}">PRODUTOS</a>
 </div>
