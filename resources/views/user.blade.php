ee028bf95225842d5b353ff4573d958335cd7dd7
thiagodevloko <thiago-limasoares@hotmail.com>
2021-05-05 19:01:52 -0300

    Alterei um nome

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,8 +1,8 @@
-<h1>USER</h1>
+{{-- <h1>USER</h1> --}}
 
 <div>
     <a href="{{route('app.user')}}">USER</a>
     <a href="{{route('app.profile')}}">PROFILE</a>
-    <a href="{{route('app')}}">APP</a>
+    <a href="{{route('app')}}">APLICATIVOS</a>
     <a href="{{route('meusprodutos')}}">PRODUTOS</a>
 </div>
